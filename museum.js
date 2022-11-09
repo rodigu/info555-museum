690ba8194da620e5ebdc0145ec4ab2877fea79c2
Rodrigo Morais <rmoraisu@protonmail.com>
2022-11-09 11:43:02 -0600

    added debug and moved movement to function

--- museum.js
+++ museum.js
@@ -42,69 +42,35 @@ function setup() {
   p5.disableFriendlyErrors = true;
   textSize(20);
   textAlign(LEFT, TOP);
+  
 }
 
 function draw() {
-  background("white");
-  image(museumMap, globalX, globalY, 1000, 1000);
-  // fill(0)
-  // ellipse(mouseX, mouseY, 20, 20)
-  // fill('red')
-  // rect(midW - playerCol.mw, midH - playerCol.mh, playerCol.w, playerCol.h)
-  image(playerSprite, midW, midH, playerDim.w, playerDim.h);
+  drawMap()
+
+  if (DO_DEBUG)
+    debugCode()
+
+  image(player.sprite, midW, midH, player.dimensions.w, player.dimensions.h);
   text(frameRate(), 0, 0);
 
   globalX += control.joystick.valX;
   globalY += control.joystick.valY;
-  let collisionBox = get(midW - playerCol.mw, midH - playerCol.mh, playerCol.w, playerCol.h)
+  let collisionBox = get(midW - player.collision.mw, midH - player.collision.mh, player.collision.w, player.collision.h)
 
   collisionBox.loadPixels()
   let collisionPixels = collisionBox.pixels
-  // console.log(collisionBox.pixels.length / 4) 1681
-  if (control.joystick.valX !== 0 && control.joystick.valY !== 0) {
-    let rightCollision = collisionPixels.slice(4 * (playerCol.w * playerCol.mh - 1), 4 * (playerCol.w * playerCol.mh - 1) + 3)
-    // rightCollision = get(midW + 21, midH).slice(0, 3);
-    if (rightCollision.reduce((a, b) => a + b, 0) <= 120) {
-      globalX += 5;
-    }
-  
-    let leftCollision = collisionPixels.slice(4 * (playerCol.w * playerCol.mh), 4 * (playerCol.w * playerCol.mh) + 3)
-    // leftCollision = get(midW - 21, midH).slice(0, 3);
-    if (leftCollision.reduce((a, b) => a + b, 0) <= 120) {
-      globalX -= 5;
-    }
-  
-    let topCollision = collisionPixels.slice(4 * playerCol.mw, 4 * playerCol.mw + 3)
-    // topCollision = get(midW, midH - 21).slice(0, 3);
-    if (topCollision.reduce((a, b) => a + b, 0) <= 120) {
-      globalY -= 5;
-    }
-  
-    let bottomCollision = collisionPixels.slice(4 * (playerCol.h * playerCol.w - playerCol.mw), 4 * (playerCol.h * playerCol.w - playerCol.mw) + 3)
-    // bottomCollision = get(midW, midH + 21).slice(0, 3);
-    // console.log([...bottomCollision])
-    if (bottomCollision.reduce((a, b) => a + b, 0) <= 120) {
-      globalY += 5;
-    }
-  }
+
+  handleMovement(collisionPixels)
 
   drawGui();
+}
 
-  // fill("red");
-  // ellipse(mouseX, mouseY, 20, 20);
-
-  // Use Joystick's output to change velocity
-
-  // console.log(
-  //   get(mouseX, mouseY)
-  //     .slice(0, 3)
-  //     .reduce((a, b) => a + b, 0)
-  // );
-
-  // Draw our ellipse
-  // rectMode(CENTER);
-  // fill("red");
-  // ellipse(midW, midH, 20, 20);
+function debugCode(){
+  fill(0)
+  ellipse(mouseX, mouseY, 20, 20)
+  fill('red')
+  rect(midW - player.collision.mw, midH - player.collision.mh, player.collision.w, player.collision.h)
 }
 
 /// Add these lines below sketch to prevent scrolling on mobile
