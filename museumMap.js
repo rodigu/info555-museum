0c8b8bb22650f57f2e13c8314e214bc65a3d94df
Rodrigo Morais <rmoraisu@protonmail.com>
2022-11-15 11:36:16 -0600

    drawArt separated from drawMap
    Improves collision detection

--- museumMap.js
+++ museumMap.js
@@ -1,5 +1,13 @@
 function drawMap(){
   background("white");
   image(assetsList.museumMap, globalX, globalY, 1000, 1000);
-  image(showing.img, globalX + showing.x, globalY + showing.y, 180, 120)
+}
+
+function drawArt(){
+  for (let artistName in artists){
+    let artist = artists[artistName]
+    for (let art of artist.artList){
+      image(art.img, globalX + art.x, globalY + art.y, 180, 120)
+    }
+  }
 }
