bedce222ff5958511af3fc353ad00fdf45ceaa17
Rodrigo Morais <rmoraisu@protonmail.com>
2022-11-16 12:40:29 -0600

    testartist updated

--- setupArtists.js
+++ setupArtists.js
@@ -1,11 +1,15 @@
 function setupArtists(artists){
   let boundingTest = getBoundingBox(-450,-440,250,520)
-  artists.testArtist = new Artist(['who are you', 'how old are you'], boundingTest)
+  artists.testArtist = new Artist([
+      new Question('who are you'),
+      new Question('how old are you')
+    ], boundingTest)
   artists.testArtist.artList.push({
-    img:loadImage("assets/job_and_his_daughters.jpg"),
+    img: loadImage("assets/job_and_his_daughters.jpg"),
     x: -320,
     y: -100,
   })
+  artists.testArtist.profileImage = loadImage('assets/pug-dog-dancing.gif')
 }
 
 function getBoundingBox(x,y,width,height){
