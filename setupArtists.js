ac797ec2a359cbbda65b352204f7a82004474287
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-12-03 10:30:55 -0600

    added carol's questions

--- setupArtists.js
+++ setupArtists.js
@@ -1,70 +1,101 @@
-function setupArtists(artists){
-  createCarol()
-  createKenshay()
-  createGabrielle()
+function setupArtists(artists) {
+  createCarol();
+  createKenshay();
+  createGabrielle();
 }
 
 function createGabrielle() {
   let gabBounding = [
-    getBoundingBox(38,-917,1200,820),
-    getBoundingBox(-395, -773, 500, 600)
-  ]
-  artists.gab = new Artist([
-    new Question('who are you', 'assets/audio/tst_audio0.wav'),
-    new Question('how old are you', 'assets/audio/tst_audio1.wav')
-  ], gabBounding)
+    getBoundingBox(38, -917, 1200, 820),
+    getBoundingBox(-395, -773, 500, 600),
+  ];
+  artists.gab = new Artist(
+    [
+      new Question("who are you", "assets/audio/tst_audio0.wav"),
+      new Question("how old are you", "assets/audio/tst_audio1.wav"),
+    ],
+    gabBounding
+  );
   artists.gab.artList.push({
     img: loadImage("assets/art/nvshu.jpeg"),
     x: -1000,
     y: -1441,
     w: 300,
-    h: 250
-  })
+    h: 250,
+  });
   artists.gab.artList.push({
     img: loadImage("assets/art/tattoo.jpeg"),
     x: -600,
     y: -1441,
     w: 250,
-    h: 250
-  })
-  artists.gab.profileImage = loadImage('assets/pug-dog-dancing.gif') // image of the artist 
+    h: 250,
+  });
+  artists.gab.profileImage = loadImage("assets/pug-dog-dancing.gif"); // image of the artist
 }
 
-function createKenshay() {
-
-}
+function createKenshay() {}
 
 function createCarol() {
   let carolBounding = [
-    getBoundingBox(38,-917,1200,820),
-    getBoundingBox(-395, -773, 500, 600)
-  ]
-  artists.carol = new Artist([
-    new Question('who are you', 'assets/audio/tst_audio0.wav'),
-    new Question('how old are you', 'assets/audio/tst_audio1.wav')
-  ], carolBounding)
+    getBoundingBox(38, -917, 1200, 820),
+    getBoundingBox(-395, -773, 500, 600),
+  ];
+  artists.carol = new Artist(
+    [
+      new Question("Who are you?", "assets/audio/carol-who_are_you.mp3"),
+      new Question(
+        "Why did you create the piece cappychilling?",
+        "assets/audio/carol-capychillling.mp3"
+      ),
+      new Question(
+        "Why did you createthe piece bacurau?",
+        "assets/audio/carol-bacural.mp3"
+      ),
+      new Question(
+        "What do you want people to know about you and your art?",
+        "assets/audio/carol-something_about_you_and_your_art.mp3"
+      ),
+      new Question(
+        "Do you struggle to enjoy your own art?",
+        "assets/audio/carol-struggling_to_like_your_own_art.mp3"
+      ),
+      new Question(
+        "What is your creative process?",
+        "assets/audio/carol-what_your_creative_process.mp3"
+      ),
+      new Question(
+        "Why do you continue to make art?",
+        "assets/audio/carol-keep_making_art.mp3"
+      ),
+      new Question(
+        "How do you want people to feel when they see your art?",
+        "assets/audio/carol-what_you_want_ppl_feel.mp3"
+      ),
+    ],
+    carolBounding
+  );
   artists.carol.artList.push({
     img: loadImage("assets/art/capychilling.jpg"),
     x: 689,
     y: -657,
     w: 250,
-    h: 250
-  })
+    h: 250,
+  });
   artists.carol.artList.push({
     img: loadImage("assets/art/bacurau.jpeg"),
     x: 200,
     y: -500,
     w: 250,
-    h: 250
-  })
-  artists.carol.profileImage = loadImage('assets/pug-dog-dancing.gif')
+    h: 250,
+  });
+  artists.carol.profileImage = loadImage("assets/pug-dog-dancing.gif");
 }
 
-function getBoundingBox(x,y,width,height){
+function getBoundingBox(x, y, width, height) {
   return {
-    x:x,
-    y:y,
-    w:width,
-    h:height
-  }
+    x: x,
+    y: y,
+    w: width,
+    h: height,
+  };
 }
